1235c2d3024e3e5d14431217f8543ab20797c684
Andrew Dang & Alisa Colon <student@epicodus.com>
2015-08-04 16:33:03 -0700

    DID NOT PASS THIRD SPEC. :|

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -2,6 +2,9 @@ class Fixnum
   define_method(:numbers_to_words) do
     final_word = nil
     double_digits = []
+    triple_digits = []
+    first_half = []
+    tens_array = []
     number_words = {  "0" => "zero",
                       "1" => "one",
                       "2" => "two",
@@ -12,16 +15,7 @@ class Fixnum
                       "7" => "seven",
                       "8" => "eight",
                       "9" => "nine",
-                      "10" => "ten",
-                      "11" => "eleven",
-                      "12" => "twelve",
-                      "13" => "thirteen",
-                      "14" => "fourteen",
-                      "15" => "fifteen",
-                      "16" => "sixteen",
-                      "17" => "seventeen",
-                      "18" => "eighteen",
-                      "19" => "nineteen" }
+                      "10" => "ten" }
 
       integars = {  0 => "zero",
                     1 => "one",
@@ -70,7 +64,29 @@ class Fixnum
       double_digits.push('0' * number_length.-(1))
       double_digits = double_digits.join()
       final_word = number_place.fetch(double_digits) + " " + number_words.fetch(number_string.join())
+
+    elsif number_length.==(3)
+      triple_digits = number_string.insert(1, "100")
     end
-    final_word
+
+    first_half.push(triple_digits.shift())
+    first_half.push(triple_digits.shift())
+    final_word = number_words.fetch(first_half[0]) + " " + number_place.fetch(first_half[1])
+
+    last_half = triple_digits.join()
+
+    last_half_length = last_half.length()
+    if last_half.to_i().<(20)
+      last_half = integars.fetch(last_half.to_i())
+
+    elsif last_half_length.==(2) && last_half.to_i>(19)
+      tens_array.push(last_half.to_s()).split('')
+      first_number = tens_array.shift()
+      double_digits.push(first_number)
+      double_digits.push('0' * number_length.-(1))
+      double_digits = double_digits.join()
+      last_half = number_place.fetch(double_digits.to_s()) + " " + number_words.fetch(number_string.to_s().join())
+    end
+    final_word + " " + last_half
   end
 end
